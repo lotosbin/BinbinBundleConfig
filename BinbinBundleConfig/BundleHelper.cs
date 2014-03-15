54f11d0124856c1e4cab76a5a817c2989e850f00
liu binbin <lotosbin@gmail.com>
2014-03-15 13:45:49 +0800

    .

---
+++ BinbinBundleConfig/BundleHelper.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace BinbinBundleConfig
+{
+    public static class BundleHelper
+    {
+        public static void AddScriptBundleAngular(this BundleCollection bundles, List<string> bowerPlugins = null)
+        {
+            Bundle bundle = new ScriptBundle("~/bundles/angular").Include("~/Scripts/angular.js", "~/Scripts/angular-animate.js", "~/Scripts/angular-cookies.js", "~/Scripts/angular-loader.js", "~/Scripts/angular-mocks.js", "~/Scripts/angular-resource.js", "~/Scripts/angular-route.js", "~/Scripts/angular-scenario.js", "~/Scripts/angular-touch.js");
+            if (null != bowerPlugins && bowerPlugins.Count > 0)
+            {
+                if (bowerPlugins.Contains("angular-loading-bar"))
+                {
+                    bundle.Include("~/bower_components/angular-loading-bar/build/loading-bar.js");
+                }
+            }
+            bundles.Add(bundle);
+        }
+
+        public static void AddStyleBundleAngular(this BundleCollection bundles, List<string> bowerPlugins = null)
+        {
+            Bundle bundle = new StyleBundle("~/Content/angular");
+            if (null != bowerPlugins && bowerPlugins.Count > 0)
+            {
+                if (bowerPlugins.Contains("angular-loading-bar"))
+                {
+                    bundle.Include("~/bower_components/angular-loading-bar/build/loading-bar.css");
+                }
+            }
+            bundles.Add(bundle);
+        }
+
+        public static void AddStyleBundleBootstrap(this BundleCollection bundles, List<string> bowerPlugins = null)
+        {
+            Bundle bundle = new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css")
+                                                                  .Include("~/Content/bootstrap-theme.css");
+            if (null != bowerPlugins && bowerPlugins.Count > 0)
+            {
+                if (bowerPlugins.Contains("angular-loading-bar"))
+                {
+                    bundle.Include("~/bower_components/angular-loading-bar/build/loading-bar.css");
+                }
+            }
+            bundles.Add(bundle);
+        }
+    }
+
+    public static class RouteHelper
+    {
+        public static void AddRouteAngular(this RouteCollection routes)
+        {
+            routes.IgnoreRoute("fonts/{*pathInfo}");
+        }
+    }
+}
---
+++ BinbinBundleConfig/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("BinbinBundleConfig")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("lotosbin")]
+[assembly: AssemblyProduct("BinbinBundleConfig")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("9d6a7547-dfd6-4891-b4ba-4bef2b5f687a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
